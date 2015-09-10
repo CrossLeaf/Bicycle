17e58c6be1d75c43df4062ff748e3cbaf6d0460c
Tsai_MengHan <pk2p007@hotmail.com.tw>
2015-09-11 02:44:27 +0800

    MapsActivity 123行有問題

--- app/src/main/java/com/menghan/bicycle/MapsActivity.java
+++ app/src/main/java/com/menghan/bicycle/MapsActivity.java
@@ -2,6 +2,7 @@ package com.menghan.bicycle;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -15,6 +16,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -25,10 +35,16 @@ public class MapsActivity extends FragmentActivity {
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private double lat[];
     private double lng[];
+    private String sna[];
+    private ArrayList<InfoList> sbi;
+    private ArrayList<InfoList> bemp;
     ImageButton listBtn;
     ImageButton markerBtn;
     ImageButton refreshBtn;
     ImageButton weatherBtn;
+    String url = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=ddb80380-f1b3-4f8e-8016-7ed9cba571d5";
+    UBikeList uBikeList = new UBikeList(); //可能有問題
+    private ArrayList<InfoList> listItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +52,9 @@ public class MapsActivity extends FragmentActivity {
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
         initViews();
+        //執行異步任務 取得連線
+        new HttpAsyncTask().execute(url);
+
         //取得 json 字串
         String json = getRowData(this, R.raw.ubike);
 //        Log.e("value", json);
@@ -44,22 +63,28 @@ public class MapsActivity extends FragmentActivity {
         //將 json 資料注入至 RetCode 物件
         RetCode code = gson.fromJson(json, RetCode.class);
         int i = 0;
-        lat = new double[381];
-        lng = new double[381];
+        lat = new double[code.getRetVal().length];
+        lng = new double[code.getRetVal().length];
+        sna = new String[code.getRetVal().length];
+        Log.e("test", String.valueOf(code.getRetVal().length));
         try {
             for (RetVal val : code.getRetVal()) {       //取得retVal的陣列裡的物件
-                Log.e("test", String.valueOf(val.getLat()));
-                Log.e("test", String.valueOf(i));
                 lat[i] = val.getLat();
+                Log.e("test", String.valueOf(val.getLat()));
                 lng[i] = val.getLng();
+                Log.e("test", String.valueOf(val.getLng()));
+                sna[i] = val.getSna();
+                Log.e("test", val.getSna());
                 i++;
+                Log.e("test", String.valueOf(i));
 //            RetVal sum = new RetVal(val.getLat(), val.getLng());   //取出要呈現的值
 //            Log.e("value", sum.toString());
-
             }
+            Log.e("test", "進入drawmarker");
             drawMarker();
+            Log.e("test", "drawMarker...");
         } catch (Exception e) {
-
+            Log.e("test", "發生錯誤...");
         }
     }
 
@@ -86,17 +111,19 @@ public class MapsActivity extends FragmentActivity {
                 break;
         }
     }
-//    private void changeFragment(Fragment f) {
-//        FragmentManager fm = getFragmentManager();
-//        FragmentTransaction transaction = fm.beginTransaction();
-//        transaction.replace(R.id.fragment_container, f);
-//        transaction.commit();
-//    }
 
     private void drawMarker() {
         Log.e("draw", String.valueOf(lat.length));
+        sbi = getListItem();
+        Log.e("draw", "取得sbi");
+        bemp = getListItem();
+        Log.e("draw", "取得bemp");
         for (int i = 0; i < lat.length; i++) {
-            mMap.addMarker(new MarkerOptions().position(new LatLng(lat[i], lng[i])).title("yes"));
+            Log.e("draw", String.valueOf(i));
+            String snippet = String.format("可借:%s 可停:%s ",sbi.get(i),bemp.get(i));
+            Log.e("draw", snippet);
+            mMap.addMarker(new MarkerOptions().position(new LatLng(lat[i], lng[i])).title(sna[i]).snippet(snippet));
+            Log.e("draw", "新增"+i+"筆");
         }
     }
 
@@ -147,5 +174,61 @@ public class MapsActivity extends FragmentActivity {
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
 
+    }
+    public class HttpAsyncTask extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... urls) {
+            try {
+                listItem = new ArrayList<>();
+                JSONObject jsonObj = new JSONObject(getJSONData(urls[0]));
+                int i = 0;
+                Log.e("list", String.valueOf(new JSONArray(new JSONObject(jsonObj.getString("result")).getString("results")).length()));
+                Log.e("list", "正在印出資料...");
+                String obj = new JSONObject(jsonObj.getString("result")).getString("results");
+                JSONObject arrayString = new JSONArray(obj).getJSONObject(i);
+                Log.e("list", obj);
+                while (i < new JSONArray(obj).length()) {
+                    String sna = arrayString.getString("sna");
+                    String ar = arrayString.getString("ar");
+                    String sbi = arrayString.getString("sbi");
+                    String bemp = arrayString.getString("bemp");
+                    InfoList list = new InfoList(sna, ar, sbi, bemp);
+                    Log.e("list", "list物件");
+                    listItem.add(list);
+                    i++;
+                    Log.e("list", String.valueOf(i));
+                }
+            } catch (Exception e) {
+            }
+            return null;
+        }
+    }
+
+    public ArrayList<InfoList> getListItem() {
+        return listItem;
+    }
+    private String getJSONData(String url) {
+        String retSrc = "";
+        HttpGet httpget = new HttpGet(url);
+        HttpClient httpclient = new DefaultHttpClient();
+        Log.e("retSrc", "讀取 JSON-1...");
+        try {
+            HttpResponse response = httpclient.execute(httpget);
+            Log.e("retSrc", "讀取 JSON-2...");
+            HttpEntity resEntity = response.getEntity();
+            if (resEntity != null) {
+                retSrc = EntityUtils.toString(resEntity);
+                Log.e("retSrc", "讀取 JSON-3...");
+            } else {
+                retSrc = "Did not work!";
+            }
+
+        } catch (Exception e) {
+            Log.e("retSrc", "讀取JSON Error...");
+            return null;
+        } finally {
+            httpclient.getConnectionManager().shutdown();
+        }
+        return retSrc;
     }
 }
--- app/src/main/java/com/menghan/bicycle/UBikeList.java
+++ app/src/main/java/com/menghan/bicycle/UBikeList.java
@@ -4,8 +4,8 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -14,105 +14,90 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 
 public class UBikeList extends Activity {
-    String url = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=ddb80380-f1b3-4f8e-8016-7ed9cba571d5";
-    private ArrayList<InfoList> listItem;
-    private InfoAdapter adapter;
+//    private ArrayList<InfoList> listItem;
     ListView listView;
-    String sna;
-    String ar;
-    String sbi;
-    String bemp;
+//    String sna;
+//    String ar;
+//    String sbi;
+//    String bemp;
+    private ListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubike_list);
         listView = (ListView) findViewById(R.id.infoList);
-        new HttpAsyncTask().execute(url);
+        MapsActivity main = new MapsActivity();
+        adapter = new InfoAdapter(UBikeList.this, main.getListItem());
+
+        listView.setAdapter(adapter);
+        listView.setTextFilterEnabled(true);
     }
 
-    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls) {
-
-            return getJSONData(urls[0]);
-        }
-
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
-            try {
-                listItem = new ArrayList<>();
-                adapter = new InfoAdapter(UBikeList.this, listItem);
-                JSONObject jsonObj = new JSONObject(result);
-                int i = 0;
-                Toast.makeText(UBikeList.this, "正在抓取資料...", Toast.LENGTH_LONG).show();
-                Log.e("list", String.valueOf(new JSONArray(new JSONObject(jsonObj.getString("result")).getString("results")).length()));
-                Log.e("list", "正在印出資料...");
-                while (i < new JSONArray(new JSONObject(jsonObj.getString("result")).getString("results")).length()) {
-                    sna = new JSONArray(new JSONObject(jsonObj.getString("result")).getString("results")).getJSONObject(i).getString("sna");
-                    ar = new JSONArray(new JSONObject(jsonObj.getString("result")).getString("results")).getJSONObject(i).getString("ar");
-                    sbi = new JSONArray(new JSONObject(jsonObj.getString("result")).getString("results")).getJSONObject(i).getString("sbi");
-                    bemp = new JSONArray(new JSONObject(jsonObj.getString("result")).getString("results")).getJSONObject(i).getString("bemp");
-                    InfoList list = new InfoList(sna, ar, sbi, bemp);
-                    listItem.add(list);
-                    i++;
-
-                }
-                Log.e("list", "adapter...");
-//                adapter = new InfoAdapter(UBikeList.this, listItem);
-                listView.setAdapter(adapter);
-                listView.setTextFilterEnabled(true);
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    public class HttpAsyncTask extends AsyncTask<String, Void, Void> {
+//        @Override
+//        protected Void doInBackground(String... urls) {
+//            try {
+//                listItem = new ArrayList<>();
+//                JSONObject jsonObj = new JSONObject(getJSONData(urls[0]));
+//                int i = 0;
+//                Log.e("list", String.valueOf(new JSONArray(new JSONObject(jsonObj.getString("result")).getString("results")).length()));
+//                Log.e("list", "正在印出資料...");
+//                String obj = new JSONObject(jsonObj.getString("result")).getString("results");
+//                JSONObject arrayString = new JSONArray(obj).getJSONObject(i);
+//                Log.e("list", obj);
+//                while (i < new JSONArray(obj).length()) {
+//                    sna = arrayString.getString("sna");
+//                    ar = arrayString.getString("ar");
+//                    sbi = arrayString.getString("sbi");
+//                    bemp = arrayString.getString("bemp");
+//                    InfoList list = new InfoList(sna, ar, sbi, bemp);
+//                    Log.e("list", "list物件");
+//                    listItem.add(list);
+//                    i++;
+//                    Log.e("list", String.valueOf(i));
+//                }
+//            } catch (Exception e) {
+//            }
+//            return null;
+//        }
+//    }
+//
+//    public ArrayList<InfoList> getListItem() {
+//        return listItem;
+//    }
 
     // 使用 HttpGet 進行連線，取得 JSON 資料
-    private String getJSONData(String url) {
-        String retSrc = "";
-        HttpGet httpget = new HttpGet(url);
-        HttpClient httpclient = new DefaultHttpClient();
-        Log.e("retSrc", "讀取 JSON-1...");
-        try {
-            HttpResponse response = httpclient.execute(httpget);
-            Log.e("retSrc", "讀取 JSON-2...");
-            HttpEntity resEntity = response.getEntity();
-            if (resEntity != null) {
-                retSrc = EntityUtils.toString(resEntity);
-                Log.e("retSrc", "讀取 JSON-3...");
-            } else {
-                retSrc = "Did not work!";
-            }
-
-        } catch (Exception e) {
-            Log.e("retSrc", "讀取JSON Error...");
-            return null;
-        } finally {
-            httpclient.getConnectionManager().shutdown();
-        }
-        return retSrc;
-    }
-
-//    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-//        String line = "";
-//        String result = "";
-//        while ((line = bufferedReader.readLine()) != null)
-//            result += line;
+//    private String getJSONData(String url) {
+//        String retSrc = "";
+//        HttpGet httpget = new HttpGet(url);
+//        HttpClient httpclient = new DefaultHttpClient();
+//        Log.e("retSrc", "讀取 JSON-1...");
+//        try {
+//            HttpResponse response = httpclient.execute(httpget);
+//            Log.e("retSrc", "讀取 JSON-2...");
+//            HttpEntity resEntity = response.getEntity();
+//            if (resEntity != null) {
+//                retSrc = EntityUtils.toString(resEntity);
+//                Log.e("retSrc", "讀取 JSON-3...");
+//            } else {
+//                retSrc = "Did not work!";
+//            }
 //
-//        inputStream.close();
-//        return result;
-//
+//        } catch (Exception e) {
+//            Log.e("retSrc", "讀取JSON Error...");
+//            return null;
+//        } finally {
+//            httpclient.getConnectionManager().shutdown();
+//        }
+//        return retSrc;
 //    }
+
+
 }
